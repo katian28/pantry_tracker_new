c187dbf0f305f37dc0933a1899e1cd47156da28f
katian28 <nkurunzizakati@gmail.com>
2024-08-04 22:09:27 -0700

    3

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { useState } from "react";
 import { firestore } from "@/firebase";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export default function Home() {
   return (
