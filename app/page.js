d85be2b4a2f5a82db254f0afa7ad755a8c43f3b8
katian28 <nkurunzizakati@gmail.com>
2024-08-05 16:10:25 -0700

    hi

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import { firestore } from "@/firebase";
 import { Box, Typography } from "@mui/material";
-
+import { deleteDoc } from "firebase/firestore";
 export default function Home() {
   const [inventory, setInventory] = useState([]);
   const [open, setOpen] = useState(false);
@@ -22,24 +22,60 @@ export default function Home() {
     });
 
     setInventory(inventoryList);
+    console.log(inventoryList)
   };
 
+  const addItem = async (item) =>{
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap.exists()){
+      const {quantity} = docSnap.data()
+      await setDoc(docRef, {quantity: quantity +1 })
+    }
+    else{
+      await setDoc(docRef, {quantity: 1})
+    }
+    await updateInventory()
+    }
+    
+ 
+  const removeItem = async (item) =>{
+    const docRef = doc(collection(firestore, 'inventory'), item)
+    const docSnap = await getDoc(docRef)
+
+    if (docSnap.exists()){
+      const {quantity} = docSnap.data()
+      if (quantity ==1){
+        await deleteDoc(docRef)
+      }
+      else{
+        await setDoc(docRef, {quantity: quantity -1 })
+      }
+    }
+    await updateInventory()
+  }
   useEffect(() => {
     updateInventory();
   }, []);
+
+const handleOpen= ()=> setOpen(true)
+const handleClose= ()=> setOpen(false)
   return (
     <Box>
       <Typography variant="h1"> Inventory Management</Typography>
       {
         inventory.forEach((item)=>{
+          console.log(item)
           return (
-            <>
+            <Box>
               {item.name}
               {item.count}
-            </>
+            </Box>
           )
         })
       }
     </Box>
-  );
-}
+  )
+
+  }
