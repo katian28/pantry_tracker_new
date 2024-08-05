5cca04bbd81b31d6e1abdbc5191f7ca6da3b43be
katian28 <nkurunzizakati@gmail.com>
2024-08-04 22:52:53 -0700

    hh

--- app/page.js
+++ app/page.js
@@ -1,14 +1,45 @@
-"use client"
+"use client";
 
-import Image from "next/image"
-import { useState } from "react"
-import { firestore } from "@/firebase"
-import {Box,Typography } from "@mui/material"
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { firestore } from "@/firebase";
+import { Box, Typography } from "@mui/material";
 
 export default function Home() {
+  const [inventory, setInventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState("");
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, "inventory"));
+    const docs = await getDocs(snapshot);
+    const inventoryList = [];
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      });
+    });
+
+    setInventory(inventoryList);
+  };
+
+  useEffect(() => {
+    updateInventory();
+  }, []);
   return (
     <Box>
-      <Typography variant= 'h1'> Inventory Management</Typography>
+      <Typography variant="h1"> Inventory Management</Typography>
+      {
+        inventory.forEach((item)=>{
+          return (
+            <>
+              {item.name}
+              {item.count}
+            </>
+          )
+        })
+      }
     </Box>
   );
 }
