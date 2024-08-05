bcbd959388411830a30cd2805334ee840ce94192
katian28 <nkurunzizakati@gmail.com>
2024-08-04 22:10:42 -0700

    nn

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-"use client";
+"use client"
 
-import Image from "next/image";
-import { useState } from "react";
-import { firestore } from "@/firebase";
-import { Box, Typography } from "@mui/material";
+import Image from "next/image"
+import { useState } from "react"
+import { firestore } from "@/firebase"
+import {Box,Typography } from "@mui/material"
 
 export default function Home() {
   return (
