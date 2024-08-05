efd9354aa995a7fa0f351f167e45b18b706d9e7d
katian28 <nkurunzizakati@gmail.com>
2024-08-04 22:07:30 -0700

    2

--- app/page.js
+++ app/page.js
@@ -1,6 +1,14 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
+import { firestore } from "@/firebase";
+import { Box } from "@mui/material";
 
 export default function Home() {
   return (
-    <>Hello World!</>);
+    <Box>
+      <Typography variant= 'h1'> Inventory Management</Typography>
+    </Box>
+  );
 }
